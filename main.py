41b050b7ab0b0b0fa9e874b741c3ea0b6d3cb356
Shreshth Tuli <shreshthtuli@gmail.com>
2021-08-23 19:59:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 from glob import glob
 import torchvision.models as models
 
+torch.manual_seed(1)
+
 def augment(trainloader, fake_data, model, epoch):
 	trainlist = list(trainloader)
 	notstart = len(fake_data)
